0faddf332408a08ed7d4ef10e5c8548617031133
Muhammad Jawaid Shamshad <mjawaid@gmail.com>
2016-01-20 01:32:20 +0500

    Testing ASP.Net MVC solution for AppHarbor deployment

---
+++ aspnet.mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(aspnet.mvc.Startup))]
+namespace aspnet.mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
