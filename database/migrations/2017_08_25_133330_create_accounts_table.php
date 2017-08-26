e4d5601b0ca8650ba08a0038fb8fb8d2a6f91950
Muhammad Jawaid Shamshad <mjawaid@gmail.com>
2017-08-27 00:37:26 +0500

    Added create accounts table migration

---
+++ database/migrations/2017_08_25_133330_create_accounts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('name');
+            $table->enum('type', ['bank', 'credit card', 'cash', 'mobile', 'other']);
+            $table->string('currency');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+}
