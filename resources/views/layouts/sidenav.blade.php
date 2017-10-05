13a3a6d87ae6b389181ab557cc530cd4a38e4e82
Muhammad Jawaid Shamshad <mjawaid@gmail.com>
2017-10-05 17:25:09 +0500

    Add side navigation

---
+++ resources/views/layouts/sidenav.blade.php
@@ -0,0 +1,23 @@
+<nav class="col-sm-3 col-md-2 d-none d-sm-block bg-light sidebar">
+    <ul class="nav nav-pills flex-column">
+        <li class="nav-item">
+            <a class="nav-link active" href="/">Dashboard <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/accounts">Accounts</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/tags">Tags</a>
+        </li>
+        
+        <li class="nav-item">
+            <a class="nav-link" href="#">Reports<small>(Coming Soon!)</small></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Budgets<small>(Coming Soon!)</small></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Forecast<small>(Coming Soon!)</small></a>
+        </li>
+    </ul>
+</nav>
