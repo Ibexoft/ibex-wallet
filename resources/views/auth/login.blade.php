227187158fb3003049a2ce3bb1d2b27c78f96ad2
Muhammad Jawaid Shamshad <jshamshad@casitron.com>
2018-10-04 18:16:22 +0500

    Register page new design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -156,7 +156,7 @@
       <div class="row align-items-center justify-content-xl-between">
         <div class="col-xl-6">
           <div class="copyright text-center text-xl-left text-muted">
-            &copy; 2018 <a href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank">Ibexoft</a>
+            &copy; 2018 <a href="https://www.ibexoft.com" class="font-weight-bold ml-1" target="_blank">Ibexoft</a>
           </div>
         </div>
         <div class="col-xl-6">
