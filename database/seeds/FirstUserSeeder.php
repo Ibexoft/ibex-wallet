6f3a1eae9386845caac84e08050a9aa9cea32e02
Muhammad Jawaid Shamshad <mjawaid@gmail.com>
2020-10-20 00:24:50 +0500

    User seeder

---
+++ database/seeds/FirstUserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class FirstUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name'              => "Jawaid",
+            'email'             => "mjawaid@gmail.com",
+            'email_verified_at' => now(),
+            'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token'    => Str::random(10),
+        ]);
+    }
+}
