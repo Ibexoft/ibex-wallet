801bee5109a4aa6cc6888c932c6ea938a7a5546d
Muhammad Jawaid Shamshad <mjawaid@gmail.com>
2020-10-23 00:24:12 +0500

    Add default categories on user signup

--- database/seeds/FirstUserCategorySeeder.php
+++ database/seeds/FirstUserCategorySeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Category;
+use App\User;
 use Illuminate\Database\Seeder;
 
 class FirstUserCategorySeeder extends Seeder
@@ -11,139 +13,9 @@ class FirstUserCategorySeeder extends Seeder
      */
     public function run()
     {
-        $categories = [
-            'Household' => [
-                'Grocery' => [
-                    'Food',
-                    'Breakfast',
-                    'Refreshment',
-                ],
-                'Toileteries and Personal Supplies',
-                'Household Supplies',
-                'Family expenses',
-                'Housing' => [
-                    'Rent',
-                    'Repair & Maintenance',
-                    'Bills' => [
-                        'Electricity',
-                        'Gas',
-                        'Water',
-                        'Internet',
-                        'Mobile balance',
-                        'Maid',
-                        'Sewerage',
-                        'Trash',
-                    ],
-                ],
-                "Toileteries and Personal Supplies",
-                "Household Supplies",
-                "Family expenses",
-                "Housing" => [
-                    "Rent",
-                    "Repair & Maintenance",
-                    "Bills" => [
-                        "Electricity",
-                        "Gas",
-                        "Water",
-                        "Internet",
-                        "Phone/Mobile bill",
-                        "Maid",
-                        "Sewerage",
-                        "Trash"
-                    ]
-                ]
-            ],
-            'Personal Care',
-            'Education' => [
-                'School Fee',
-                'Stationary, Books, & Supplies',
-                'Uniforms, Suits',
-                'Qari, Tution',
-            ],
-            'Healthcare' => [
-                'Doctor',
-                'Medicine',
-            ],
-            'Entertainment' => [
-                'Restaurant',
-                'Cafe',
-                'Order Food',
-                'Outing, Picnic',
-            ],
-            'Vehicle' => [
-                'Fuel' => [
-                    'Petrol',
-                    'CNG',
-                    'Diesel',
-                ],
-                'Maintenance',
-                'Repair',
-                'Taxes and Documentation',
-            ],
-            'Religious' => [
-                'Zakat',
-                'Sadqa',
-                'Qurbani',
-                'Hajj, Umrah',
-            ],
-            "Shopping" => [
-                "Apparel",
-                "Beauty, Cosmetics, Jewelry, & Accessories",
-                "Gadgets, Devices, & Accessories",
-                "Toys & Kids items",
-                "Other"
-            ],
-            'Gifts',
-            'Home Renovation & Decor',
-            'Transportation' => [
-                'Parking',
-                'Tolls',
-                'Taxi',
-            ],
-            'Other' => [
-                'Bank and ATM Fee',
-            ],
-            'Income' => [
-                'Job, Salary',
-                'Profit on business',
-                'Sell',
-                'Other',
-            ],
-        ];
-
-        $this->create(App\User::first()->id, $categories);
-    }
-
-    protected function create(int $userId, array $subCategories = null, $category = null, App\Category $parentCategory = null)
-    {
-        $parentCat = null;
-
-        if ($category) {
-            $found = App\Category::where('user_id', $userId)
-                        ->where('name', $category)
-                        ->where('parent_category_id', $parentCategory ? $parentCategory->id : null)
-                        ->get()
-                        ->count();
-
-            if ($found > 0) {
-                return;
-            }
-
-            $parentCat = App\Category::create([
-                'user_id'               => $userId,
-                'name'                  => $category,
-                'parent_category_id'    => $parentCategory ? $parentCategory->id : null,
-            ]);
-        }
-
-        if (is_array($subCategories)) {
-            foreach ($subCategories as $categoryName => $subCats) {
-                if (is_array($subCats)) {
-                    $this->create($userId, $subCats, $categoryName, $parentCat);
-                    continue;
-                }
-                $this->create($userId, null, $subCats, $parentCat);
-            }
-        }
+        Category::bulkCreateForUser(
+            User::first()->id,
+            config('defaultcategories')
+        );
     }
 }
