fbf5f4101a862dba6f25042d7e00d41f432bfe10
Faiz Ahmed <84289705+faizahmed1234@users.noreply.github.com>
2024-08-28 13:27:31 +0500

    XW-55 Feat: Design layouts for accounts crud operations (#77)
    * XW-55 Feet:Design layouts for create and update accounts
    
    * XW-55 - Feat: Design layout for delete account
    
    * XW-55 - Feat: Design layout for accounts view
    
    * XW-55 Feat: Implement frontend and backend validation
    
    * XW-55 - Optimize the js code

---
+++ config/custom.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'account_types' => [
+        1 => 'Cash',
+        2 => 'General',
+        3 => 'Current Account',
+    ],
+    'currencies' => [
+        'PKR' => 'PKR',
+        'USD' => 'USD',
+        // Add more currencies as needed
+    ],
+];
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
     <link rel="icon" type="image/png" href="../assets/img/favicon.png">
     <title>
@@ -140,7 +141,7 @@
     <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script src="{{asset('assets/js/custom.js')}}"></script>
+    <script src="{{asset('assets/js/custom.js?v=1.0.0')}}"></script>
     @yield('script')
 </body>
 
