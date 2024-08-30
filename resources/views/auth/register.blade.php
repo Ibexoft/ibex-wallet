8fb46b2dfd674dc21321b75d503d065009ba12e2
Muhammad Maaz Faisal <m.maazfaisal0301@gmail.com>
2024-08-30 17:42:19 +0500

    XW-53 feat: Updated Authentication pages UI

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,68 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
-
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+@extends('layouts.app')
+@section('content')
+    <section>
+        <div class="page-header min-vh-100">
+            <div class="container">
+                <form method="POST" action="{{ route('register') }}">
+                    @csrf
+                <div class="row">
+                    <div class="col-xl-4 col-lg-5 col-md-7 d-flex flex-column mx-lg-0 mx-auto">
+                        <div class="card card-plain">
+                            <div class="ps-4 pb-0 text-left">
+                                <h4 class="font-weight-bolder">Register</h4>
+                                <p class="mb-0">Enter your details to register</p>
+                            </div>
+                            <div class="card-body">
+                                <form role="form">
+                                    <div class="mb-3">
+                                        <input type="text" class="form-control form-control-lg" placeholder="Name" name="name"
+                                            aria-label="Name" aria-describedby="name-addon">
+                                    </div>
+                                    <div class="mb-3">
+                                        <input type="email" class="form-control form-control-lg" placeholder="Email" name="email"
+                                            aria-label="Email" aria-describedby="email-addon">
+                                    </div>
+                                    <div class="mb-3">
+                                        <input type="password" class="form-control form-control-lg" placeholder="Password" name="password"
+                                            aria-label="Password" aria-describedby="password-addon">
+                                    </div>
+                                    <div class="mb-3">
+                                        <input type="password" class="form-control form-control-lg" placeholder="Confirm Password" name="password_confirmation"
+                                            aria-label="Confirm  Password" aria-describedby="confirm-password-addon">
+                                    </div>
+                                    <div class="text-center">
+                                        <button type="submit"
+                                            class="btn btn-lg bg-gradient-primary btn-lg w-100 mt-4 mb-0">Register</button>
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="card-footer text-center pt-0 px-lg-2 px-1">
+                                <p class="mb-4 text-sm mx-auto">
+                                    Already have an account?
+                                    <a href="{{route('login')}}"
+                                     class="text-primary text-gradient font-weight-bold">Sign in</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div
+                        class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 end-0 text-center justify-content-center flex-column">
+                        <div
+                            class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center">
+                            <img src="{{ asset('assets/img/shapes/pattern-lines.svg') }}" alt="pattern-lines"
+                                class="position-absolute opacity-4 start-0">
+                            <div class="position-relative">
+                                <img class="max-width-500 w-100 position-relative z-index-2"
+                                    src="{{ asset('assets/img/illustrations/chat.webp') }}">
+                            </div>
+                            <h4 class="mt-5 text-white font-weight-bolder">"Attention is the new currency"</h4>
+                            <p class="text-white">The more effortless the writing looks, the more effort the writer actually
+                                put into the process.</p>
+                        </div>
+                    </div>
+                </div>
+                </form>
+            </div>
         </div>
-
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+    </section>
+@endsection
