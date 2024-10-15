b697c4a8aaf6ea0363c8e68ccfeac6351d18c71f
mjawaid <8084794+mjawaids@users.noreply.github.com>
2024-10-15 04:45:13 +0000

    Remove unit tests

--- tests/Feature/Auth/PasswordResetTest.php
+++ tests/Feature/Auth/PasswordResetTest.php
@@ -12,62 +12,62 @@ class PasswordResetTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_reset_password_link_screen_can_be_rendered(): void
-    {
-        $response = $this->get('/forgot-password');
+    // public function test_reset_password_link_screen_can_be_rendered(): void
+    // {
+    //     $response = $this->get('/forgot-password');
 
-        $response->assertStatus(200);
-    }
+    //     $response->assertStatus(200);
+    // }
 
-    public function test_reset_password_link_can_be_requested(): void
-    {
-        Notification::fake();
+    // public function test_reset_password_link_can_be_requested(): void
+    // {
+    //     Notification::fake();
 
-        $user = User::factory()->create();
+    //     $user = User::factory()->create();
 
-        $this->post('/forgot-password', ['email' => $user->email]);
+    //     $this->post('/forgot-password', ['email' => $user->email]);
 
-        Notification::assertSentTo($user, ResetPassword::class);
-    }
+    //     Notification::assertSentTo($user, ResetPassword::class);
+    // }
 
-    public function test_reset_password_screen_can_be_rendered(): void
-    {
-        Notification::fake();
+    // public function test_reset_password_screen_can_be_rendered(): void
+    // {
+    //     Notification::fake();
 
-        $user = User::factory()->create();
+    //     $user = User::factory()->create();
 
-        $this->post('/forgot-password', ['email' => $user->email]);
+    //     $this->post('/forgot-password', ['email' => $user->email]);
 
-        Notification::assertSentTo($user, ResetPassword::class, function ($notification) {
-            $response = $this->get('/reset-password/'.$notification->token);
+    //     Notification::assertSentTo($user, ResetPassword::class, function ($notification) {
+    //         $response = $this->get('/reset-password/'.$notification->token);
 
-            $response->assertStatus(200);
+    //         $response->assertStatus(200);
 
-            return true;
-        });
-    }
+    //         return true;
+    //     });
+    // }
 
-    public function test_password_can_be_reset_with_valid_token(): void
-    {
-        Notification::fake();
+    // public function test_password_can_be_reset_with_valid_token(): void
+    // {
+    //     Notification::fake();
 
-        $user = User::factory()->create();
+    //     $user = User::factory()->create();
 
-        $this->post('/forgot-password', ['email' => $user->email]);
+    //     $this->post('/forgot-password', ['email' => $user->email]);
 
-        Notification::assertSentTo($user, ResetPassword::class, function ($notification) use ($user) {
-            $response = $this->post('/reset-password', [
-                'token' => $notification->token,
-                'email' => $user->email,
-                'password' => 'password',
-                'password_confirmation' => 'password',
-            ]);
+    //     Notification::assertSentTo($user, ResetPassword::class, function ($notification) use ($user) {
+    //         $response = $this->post('/reset-password', [
+    //             'token' => $notification->token,
+    //             'email' => $user->email,
+    //             'password' => 'password',
+    //             'password_confirmation' => 'password',
+    //         ]);
 
-            $response
-                ->assertSessionHasNoErrors()
-                ->assertRedirect(route('login'));
+    //         $response
+    //             ->assertSessionHasNoErrors()
+    //             ->assertRedirect(route('login'));
 
-            return true;
-        });
-    }
+    //         return true;
+    //     });
+    // }
 }
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -9,8 +9,8 @@ class ExampleTest extends TestCase
     /**
      * A basic test example.
      */
-    public function test_that_true_is_true(): void
-    {
-        $this->assertTrue(true);
-    }
+    // public function test_that_true_is_true(): void
+    // {
+    //     $this->assertTrue(true);
+    // }
 }
