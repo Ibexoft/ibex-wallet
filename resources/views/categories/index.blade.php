a9dab6fb0a772ea4c7a354ae57e0395716d7e7fe
Hamza Bham <92208535+MHamzaBham@users.noreply.github.com>
2024-10-18 16:37:26 +0500

    XW-92 fix: Removed progress bar from categories

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -14,7 +14,7 @@
                 <div class="category-card bg-white m-auto position-relative row align-items-center justify-content-between py-2 px-0 rounded my-3 shadow-sm" id="category-{{$category->id}}">
                     <!-- Column 1: Category Info -->
                     <div onclick="toggleCategory.call(this)"
-                        class="dropdown-toggler col-lg-5 col-md-5 col-10 d-flex align-items-center opacity-10"
+                        class="dropdown-toggler col-lg-6 col-md-8 col-10 d-flex align-items-center opacity-10"
                         style="{{ count($category->subcategories) ? 'cursor: pointer;' : '' }}">
                         <div class="col-auto icon icon-sm icon-shape bg-gradient-primary shadow text-center border-radius-md">
                             <i class="fa {{$category->icon}} opacity-10" aria-hidden="true" style="font-size: 0.85rem;"></i>
@@ -29,17 +29,8 @@
                         </div>
                     </div>
 
-                    <!-- Column 2: Progress Bar -->
-                    <div class="col-lg-2 col-md-3 text-center d-md-block d-none" title="Category Share">
-                        <p class="p-0 m-0 text-sm">40%</p>
-                        <div class="progress d-flex align-items-center" style="height: 8px;">
-                            <div class="progress-bar bg-gradient-primary" style="width: 40%; height: 100%;">
-                            </div>
-                        </div>
-                    </div>
-
-                    <!-- Column 3: Options Dropdown -->
-                    <div class="col-lg-5 col-md-4 col-2 text-end category-options">
+                    <!-- Column 2: Options Dropdown -->
+                    <div class="col-lg-6 col-md-4 col-2 text-end category-options">
                         <div class="filter z-5">
                             <a class="icon text-primary" href="#" data-bs-toggle="dropdown">
                                 <i class="fa fa-ellipsis-h py-1"></i>
