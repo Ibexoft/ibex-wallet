7d1a5d6403dd0d803e01f2849769142307eb9f23
Muhammad Maaz Faisal <73815553+MuhammadMaazFaisal@users.noreply.github.com>
2024-10-21 19:44:35 +0500

    XW-93 Fix: Sub menus fonts are inconsistent (#102)

--- resources/views/categories/dropdownMenu.blade.php
+++ resources/views/categories/dropdownMenu.blade.php
@@ -2,7 +2,7 @@
     <!-- Add Subcategory if parentable -->
     @if ($isParentable ?? false)
         <li>
-            <a class="dropdown-item py-1 small" href="#" data-bs-toggle="modal"
+            <a class="dropdown-item py-1" href="#" data-bs-toggle="modal"
                data-bs-target="#addCategoryModal"
                onclick="setCategory({{ $categoryId }}, null, true)">
                Add subcategory
@@ -13,14 +13,14 @@
     <!-- Edit Category -->
     <li data-bs-toggle="modal" data-bs-target="#editCategoryModal"
         onclick="setCategory({{ $categoryId }}, '{{ $categoryName }}', {{ $isSubcategory }})">
-        <a class="dropdown-item py-1 small" href="#">
+        <a class="dropdown-item py-1" href="#">
             Edit
         </a>
     </li>
 
     <!-- Delete Category -->
     <li onclick="deleteCategory({{ $categoryId }}, {{ $parentId ?? 'null' }})" class="delete-option {{count($subcategories) ? 'd-none' : ''}}" >
-        <a class="dropdown-item py-1 small" href="#">
+        <a class="dropdown-item py-1" href="#">
                 Delete
         </a>
     </li>
