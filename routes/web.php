800ec41a838d45ea9c61254eed01638bc5a5961e
Hamza Bham <92208535+MHamzaBham@users.noreply.github.com>
2024-12-30 21:31:30 +0500

    Profile Page

--- routes/web.php
+++ routes/web.php
@@ -10,15 +10,13 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
     Route::get('/', [HomeController::class, 'index'])->name('dashboard');
     Route::resource('transactions', TransactionController::class);
     Route::resource('accounts', AccountController::class);
     Route::resource('categories', CategoryController::class);
     Route::resource('wallets', WalletController::class);
+    Route::resource('profile', ProfileController::class);
+    Route::put('/profile/{profile}/update-image', [ProfileController::class, 'updateImage'])->name('profile.updateImage');
 });
 
 
